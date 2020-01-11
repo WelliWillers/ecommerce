f958c514ed944ae670cdbe9fb6467047a96f0685
WelliWillers <wellington.willers@gmail.com>
2020-01-10 21:10:26 -0300

    inicio cadastro de produtos

---
+++ admin-products.php
@@ -0,0 +1,79 @@
+<?php
+
+use \Hcode\Page;
+use \Hcode\PageAdmin;
+use \Hcode\Model\User;
+use \Hcode\Model\Category;
+use Hcode\Model\Product;
+
+$app->get("/admin/products", function(){
+
+    User::verifyLogin();
+
+    $products = Product::listAll();
+    
+    $page = new PageAdmin();
+
+    $page->setTpl("products", [
+        "products"=>$products
+    ]);
+
+});
+
+$app->get('/admin/products/create', function(){
+
+	User::verifyLogin();
+
+	$page = new PageAdmin();
+
+	$page->setTpl('products-create');
+
+});
+
+$app->post('/admin/products/create', function(){
+
+	User::verifyLogin();
+
+	$products = new Product();
+
+	$products->setData($_POST);
+
+	$products->save();
+
+	header("Location: /admin/products");
+	exit;
+
+});
+
+$app->get('/admin/products/:idproduct/delete', function($idproduct){
+
+	User::verifyLogin();
+
+	$products = new Product();
+
+	$products->get((int)$idproduct);
+
+	$products->delete();
+
+	header("Location: /admin/products");
+	exit;
+
+});
+
+$app->get('/admin/products/:idproduct', function($idproduct){
+
+	User::verifyLogin();
+
+	$products = new Product();
+
+	$products->get((int)$idproduct);
+
+	$page = new PageAdmin();
+
+	$page->setTpl('products-update', array(
+		"product"=>$products->getValues()
+	));
+
+});
+
+?>
